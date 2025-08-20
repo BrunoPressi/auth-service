fd08e78cef78d249c1bb9ac2c62936236617e514
Bruno Pressi <brunopressi2012@gmail.com>
2025-08-20 15:33:02 -0300

    feat: add UserDetailsServiceImpl class

---
+++ src/main/java/com/auth/service/service/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.auth.service.service;
+
+import com.auth.service.entity.User;
+import com.auth.service.entity.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByEmail(username);
+        return new UserDetailsImpl(user);
+    }
+}
--- src/main/java/com/auth/service/service/UserService.java
+++ src/main/java/com/auth/service/service/UserService.java
@@ -1,7 +1,9 @@
 package com.auth.service.service;
 
+import com.auth.service.entity.User;
 import com.auth.service.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +11,11 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException(String.format("User |%s| not found", email))
+        );
+    }
 
 }
