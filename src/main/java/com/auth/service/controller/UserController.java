a66ff62f9365edf29539e3ed594eca3b9ccf124b
Bruno Pressi <brunopressi2012@gmail.com>
2025-08-20 16:00:48 -0300

    feat: add JwtTokenService class

--- src/main/java/com/auth/service/controller/UserController.java
+++ src/main/java/com/auth/service/controller/UserController.java
@@ -1,15 +1,9 @@
 package com.auth.service.controller;
 
-import com.auth.service.dto.UserCreateDTO;
-import com.auth.service.entity.User;
 import com.auth.service.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
