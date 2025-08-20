7a95a6fed910e47390ed59732bf3623a88e71209
Bruno Pressi <brunopressi2012@gmail.com>
2025-08-20 14:22:12 -0300

    feat: add mapping User and User Repository interface

--- src/main/java/com/auth/service/entity/User.java
+++ src/main/java/com/auth/service/entity/User.java
@@ -3,13 +3,20 @@ package com.auth.service.entity;
 import com.auth.service.entity.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
-public class User {
+@Document(collection = "users")
+public class User implements Serializable {
+
+    @Id
+    private String id;
     private String email;
     private String password;
     private List<Role> roles = new ArrayList<>();
