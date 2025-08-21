42c007ec464690d60321b40120039ba1a5f83ee3
Bruno Pressi <brunopressi2012@gmail.com>
2025-08-21 14:16:56 -0300

    feat: add authenticateUser method

--- src/main/java/com/auth/service/service/UserService.java
+++ src/main/java/com/auth/service/service/UserService.java
@@ -1,8 +1,14 @@
 package com.auth.service.service;
 
+import com.auth.service.dtos.JwtTokenDTO;
+import com.auth.service.dtos.UserLoginDTO;
 import com.auth.service.entity.User;
+import com.auth.service.entity.UserDetailsImpl;
 import com.auth.service.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +17,23 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenService jwtTokenService;
 
     public User findByEmail(String email) {
         return userRepository.findByEmail(email).orElseThrow(
                 () -> new UsernameNotFoundException(String.format("User |%s| not found", email))
         );
+    }
+
+    public JwtTokenDTO authenticateUser(UserLoginDTO userLoginDTO) {
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(userLoginDTO.getEmail(), userLoginDTO.getPassword());
+
+        Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        return new JwtTokenDTO(jwtTokenService.generateToken(userDetails));
     }
 
 }
