8fafd895a1932e48454ee37d3c5e0d8661765eaf
Bruno Pressi <brunopressi2012@gmail.com>
2025-08-21 14:11:10 -0300

    feat: add UserLoginDTO and JwtTokenDTO classes

---
+++ src/main/java/com/auth/service/dtos/UserLoginDTO.java
@@ -0,0 +1,21 @@
+package com.auth.service.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginDTO {
+
+    @Pattern(regexp = "^[a-zA-Z0-9_'^&/+-]+(?:\\.[a-zA-Z0-9_'^&/+-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}$", message = "Invalid Email")
+    private String email;
+
+    @Size(min = 6, max = 12)
+    private String password;
+
+}
