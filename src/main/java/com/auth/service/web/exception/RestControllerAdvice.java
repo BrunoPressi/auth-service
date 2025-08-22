9d5c5689fbef77ff83cefc7cb093c2fed84dd96d
Bruno Pressi <brunopressi2012@gmail.com>
2025-08-22 14:02:06 -0300

    feat: add ObjectMapper config and JwtAuthenticationEntryPoint classes

--- src/main/java/com/auth/service/web/exception/RestControllerAdvice.java
+++ src/main/java/com/auth/service/web/exception/RestControllerAdvice.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -33,6 +34,29 @@ public class RestControllerAdvice {
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST.value())
+                .body(exceptionHandler);
+
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<com.auth.service.exception.ExceptionHandler> usernameNotFoundException(HttpServletRequest request, RuntimeException e) {
+
+        LocalDateTime timestamp = LocalDateTime.now();
+        String path = request.getRequestURI();
+        String errorMessage = e.getMessage();
+        String statusMessage = HttpStatus.UNAUTHORIZED.getReasonPhrase();
+        int statusCode = HttpStatus.UNAUTHORIZED.value();
+
+        com.auth.service.exception.ExceptionHandler exceptionHandler = new com.auth.service.exception.ExceptionHandler(
+                timestamp,
+                path,
+                errorMessage,
+                statusMessage,
+                statusCode
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED.value())
                 .body(exceptionHandler);
 
     }
